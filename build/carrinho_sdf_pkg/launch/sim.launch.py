704dcfd8b080759470ca5583c397a240c2264996
Larissa <larissarochasantos15@gmail.com>
2025-10-13 13:27:44 -0500

    add stl

--- build/carrinho_sdf_pkg/launch/sim.launch.py
+++ build/carrinho_sdf_pkg/launch/sim.launch.py
@@ -1,1 +1,1 @@
-/home/vboxuser/Gemeo_digital_MJV/src/carrinho_sdf_pkg/launch/sim.launch.py
+/home/larissa/Gemeo_digital_MJV/src/carrinho_sdf_pkg/launch/sim.launch.py
--- build/carrinho_sdf_pkg/share/carrinho_sdf_pkg/hook/pythonpath_develop.sh
+++ build/carrinho_sdf_pkg/share/carrinho_sdf_pkg/hook/pythonpath_develop.sh
@@ -1,3 +1,3 @@
 # generated from colcon_core/shell/template/hook_prepend_value.sh.em
 
-_colcon_prepend_unique_value PYTHONPATH "/home/vboxuser/Gemeo_digital_MJV/build/carrinho_sdf_pkg"
+_colcon_prepend_unique_value PYTHONPATH "/home/larissa/Gemeo_digital_MJV/build/carrinho_sdf_pkg"
